4e93363a916f639b79eb5290a3104ac8b941dadf
Austin Graves <austingraves106@aol.com>
2019-07-30 20:44:17 -0500

    learning JavaScript

---
+++ Projects/Day 7 (7:30:19)/JS/alerts-prompts-confirmations.js
@@ -0,0 +1,8 @@
+
+// confirm asks you a yes or no question. 
+var doYouRock = confirm('The question is, do you rock?');
+// prompts allow a response from the user
+var howMuchRock = prompt('How much do you rock?');
+
+alert(doYouRock);
+alert(howMuchRock);
