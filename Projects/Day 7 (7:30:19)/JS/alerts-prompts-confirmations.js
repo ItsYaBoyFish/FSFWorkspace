7641519fe324cdc116ef9ba0c8abecee3613d71d
Austin Graves <austingraves106@aol.com>
2019-08-01 18:41:28 -0500

    Update

--- Projects/Day 7 (7:30:19)/JS/alerts-prompts-confirmations.js
+++ Projects/Day 7 (7:30:19)/JS/alerts-prompts-confirmations.js
@@ -1,8 +1,24 @@
 
 // confirm asks you a yes or no question. 
-var doYouRock = confirm('The question is, do you rock?');
+  //var doYouRock = confirm('The question is, do you rock?');
 // prompts allow a response from the user
-var howMuchRock = prompt('How much do you rock?');
+  //var howMuchRock = prompt('How much do you rock?');
 
-alert(doYouRock);
-alert(howMuchRock);
+//alert(doYouRock);
+//alert(howMuchRock);
+
+// Document.write functionality
+document.write('Something Cool');
+
+foodArray = [
+  combo1 = {
+    mainCourse: "Chicken Nuggets",
+    sideItem: "French Fries"
+  },
+  combo2 = {
+    mainCourse: 'Cheeseburger',
+    sideItem: 'French Fries'
+  }
+]
+
+console.log(foodArray[1].mainCourse);
